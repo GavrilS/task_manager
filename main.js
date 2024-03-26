a8f68c72d929993ff878b5c30d6b40b914487821
GavrilS <gavr1lll@abv.bg>
2024-03-26 22:08:19 +0200

    Added the basic popup elements for the create tasks action

---
+++ main.js
@@ -0,0 +1,7 @@
+const createTask = document.querySelector('#create-task');
+const saveTasks = document.querySelector('#save-task');
+const loadTasks = document.querySelector('#load-task');
+
+createTask.addEventListener('click', () => {
+    
+})
