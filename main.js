a4d84ff1e91f744c892063dbaf73df3cb7847c50
GavrilS <gavr1lll@abv.bg>
2024-04-09 23:54:37 +0300

    Added functionality to buttons

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const createTask = document.querySelector('#create-task');
 const saveTasks = document.querySelector('#save-task');
 const loadTasks = document.querySelector('#load-task');
+const closePopup = document.querySelector('.popup-btn-close');
+
+const newTaskForm = document.getElementById('create-task-modal');
 
 createTask.addEventListener('click', () => {
-    
+    newTaskForm.style.display = 'block';
+    newTaskForm.style.visibility = 'visible';
+    console.log(newTaskForm);
+})
+
+closePopup.addEventListener('click', () => {
+    newTaskForm.style.display = 'none';
 })
