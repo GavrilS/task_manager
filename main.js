e13ba2115195d7a14e4da2399ffc325809a2151a
GavrilS <gavr1lll@abv.bg>
2024-04-11 21:59:50 +0300

    Added opacity to the background elements when the modal for creating a new task is called

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const mainComponent = document.querySelector('.main-component');
 const createTask = document.querySelector('#create-task');
 const saveTasks = document.querySelector('#save-task');
 const loadTasks = document.querySelector('#load-task');
@@ -6,11 +7,12 @@ const closePopup = document.querySelector('.popup-btn-close');
 const newTaskForm = document.getElementById('create-task-modal');
 
 createTask.addEventListener('click', () => {
+    mainComponent.style.opacity = 0.5;
     newTaskForm.style.display = 'block';
-    newTaskForm.style.visibility = 'visible';
     console.log(newTaskForm);
 })
 
 closePopup.addEventListener('click', () => {
     newTaskForm.style.display = 'none';
+    mainComponent.style.opacity = 1;
 })
