a9fa7e44650fc3aba94035c2c4792cb940cb2fa3
GavrilS <gavr1lll@abv.bg>
2024-04-11 23:33:43 +0300

    Get values for new task cards from modal

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const createTask = document.querySelector('#create-task');
 const saveTasks = document.querySelector('#save-task');
 const loadTasks = document.querySelector('#load-task');
 const closePopup = document.querySelector('.popup-btn-close');
+const saveTaskModal = document.querySelector('#create-new-task');
 
 const newTaskForm = document.getElementById('create-task-modal');
 
@@ -15,4 +16,16 @@ createTask.addEventListener('click', () => {
 closePopup.addEventListener('click', () => {
     newTaskForm.style.display = 'none';
     mainComponent.style.opacity = 1;
+})
+
+saveTaskModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    // console.log(e);
+    const taskFormChildren = newTaskForm.children;
+    const title = taskFormChildren[0].lastElementChild.value;
+    const description = taskFormChildren[1].lastElementChild.value;
+    const status = taskFormChildren[2].lastElementChild.value;
+
+    console.log(title);
+    console.log(description);
 })
