768ed39822102166b3cdc439cbbdfad4a4ed96e8
GavrilS <gavr1lll@abv.bg>
2024-04-13 18:27:48 +0300

    Added task module

---
+++ js_modules/tasks.js
@@ -0,0 +1,42 @@
+class Task {
+    constructor(title, description, status) {
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get description() {
+        return this._description;
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    set title(title) {
+        if (title) {
+            this._title = title;
+        }
+    }
+
+    set description(description) {
+        if (description) {
+            this._description = description;
+        }
+    }
+
+    set status(status) {
+        if (status) {
+            this._status = status;
+        }
+    }
+}
+
+
+export {
+    Task
+};
