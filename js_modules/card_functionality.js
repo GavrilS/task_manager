35e3d87bfd59a0de02f1e3449f71d148b5c89bbc
GavrilS <gavr1lll@abv.bg>
2024-04-25 21:05:48 +0300

    Fixing the opening/closing card popup logic

--- js_modules/card_functionality.js
+++ js_modules/card_functionality.js
@@ -6,18 +6,37 @@ function getTaskParent(element) {
     }
 }
 
+function getCardTask(element) {
+    if (element.id === 'card-container') {
+        const task = {};
+        for (const child of element.children) {
+            console.log(child);
+            task[child.id] = child.innerHTML;
+        }
+        console.log('task: ', task);
+        return task;
+    }
+    return getCardTask(element.firstChild);
+}
+
 
 function focusCardOnClick(cardElem, mainComponent) {
     mainComponent.style.opacity = 0.5;
-    cardElem.classList.add('card-focus');
-    // cardElem.style.opacity = 1;
+    const cardTask = getCardTask(cardElem);
+    const cardPopup = document.querySelector('#card-popup');
+    for (const child of cardPopup.children) {
+        child.innerHTML = cardTask[child.id];
+    }
+    cardPopup.style.display = 'block';
 }
 
 
 function removeCardFocus(mainComponent) {
-    const focusCard = document.querySelector('card-focus');
-    if (focusCard) {
-        focusCard.classList.remove('card-focus');
+    // const focusCard = document.querySelector('card-focus');
+    const cardPopup = document.querySelector('#card-popup')
+    if (cardPopup) {
+        // focusCard.classList.remove('card-focus');
+        cardPopup.style.display = 'none';
         mainComponent.style.opacity = 1;
     }
 }
